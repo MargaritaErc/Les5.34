4596cf5a38b09c8a6000fe4a0fde7599ec9cc756
MargaritaErc <118373440+MargaritaErc@users.noreply.github.com>
2022-11-30 16:02:17 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+// Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+
+int[] array = new int[10];
+int count = 0;
+int[] GetArray(int[]array)
+{for (int i=0; i < array.Length; i++)
+{array[i] = new Random().Next(99,1000);
+if (array[i] % 2 ==0)
+{count++;
+}
+}
+return array;
+}
+GetArray(array);
+Console.WriteLine(String.Join("", array));
+Console.WriteLine("Кол-во четных чисел в массиве"  +count);
